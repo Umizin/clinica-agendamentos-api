4bbdec8025f957c4668944bbe540806b9bf96a8b
Paulo Silva <pv_silva@id.uff.br>
2026-06-17 12:42:11 -0300

    fix(web): corrige nginx para deploy no render

--- services/web/docker-entrypoint.sh
+++ services/web/docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
+
+PORT=${PORT:-8080}
 GATEWAY_URL=${GATEWAY_URL:-http://gateway:3000}
+
 case "$GATEWAY_URL" in
   http*) ;;
   *) GATEWAY_URL="http://${GATEWAY_URL}" ;;
 esac
-sed "s|GATEWAY_URL|${GATEWAY_URL}|g" /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+sed "s|GATEWAY_URL|${GATEWAY_URL}|g; s|LISTEN_PORT|${PORT}|g" \
+  /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
 exec nginx -g 'daemon off;'
